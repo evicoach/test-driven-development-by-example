a6e4399272672b94bb5837f961a6cb1dda1f8501
Evi Owhotemu <evicoach@gmail.com>
2022-09-03 06:52:54 +0100

    Added currency multiplication test

---
+++ src/main/java/com/columnhack/Dollar.java
@@ -0,0 +1,12 @@
+package com.columnhack;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier){
+        this.amount *= multiplier;
+    }
+}
